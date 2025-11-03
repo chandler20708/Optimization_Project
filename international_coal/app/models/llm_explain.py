3c4d4311ecbf7cfa7f3e2e677300c041f0418d97
chandlerliuliu <chandlerliu@tmrmds.co>
2025-11-03 03:57:33 +0000

    cleanup google

--- international_coal/app/models/llm_explain.py
+++ international_coal/app/models/llm_explain.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-from google import genai
+# from google import genai
 
-client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+# client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 
-@st.cache_data
-def explain_model_results(prompt: str, df: pl.DataFrame) -> genai.types.GenerateContentResponse:
-    response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        contents=f"{prompt}: {str(df)}"
-    )
-    return response
+# @st.cache_data
+# def explain_model_results(prompt: str, df: pl.DataFrame) -> genai.types.GenerateContentResponse:
+#     response = client.models.generate_content(
+#         model="gemini-2.5-flash-lite",
+#         contents=f"{prompt}: {str(df)}"
+#     )
+#     return response
