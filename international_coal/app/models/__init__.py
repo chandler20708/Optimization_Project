0a9962d9a69780e31f186509f3ae20a30bad4ca3
chandlerliuliu <chandlerliu@tmrmds.co>
2025-11-14 16:04:03 +0000

    debug

--- international_coal/app/models/__init__.py
+++ international_coal/app/models/__init__.py
@@ -1,7 +1,7 @@
 from .model import run_model
-# from .llm_explain import explain_model_results
+from .llm_explain import explain_model_results
 
 __all__ = [
   "run_model",
-  # "explain_model_results",
+  "explain_model_results",
 ]
